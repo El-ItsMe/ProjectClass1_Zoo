edc3d1821bf61f56c338aa57010611bec8f83eae
El-ItsMe <elnazteymurova001@gmail.com>
2022-10-26 03:52:40 +0400

    Initial commit

---
+++ ProjectClass1_Zoo/ProjectClass1_Zoo/Program.cs
@@ -0,0 +1,16 @@
+﻿using ProjectClass1_Zoo;
+
+Frog a = new Frog();
+a.Name = "Po";
+
+Console.WriteLine(a.Name);
+a.Croak();
+
+Frog b = new Frog();
+b.NameOfAnimalSpecies = "Bear";
+
+Frog c = new Frog();
+c.Biom = "Tropics";
+
+Frog area = new Frog();
+area.RequiredArea = "52x80 m^2";
