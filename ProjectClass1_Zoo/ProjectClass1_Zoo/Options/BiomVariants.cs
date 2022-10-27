1e757f0448085fe096d8b554aefdb0fdc5ade5d7
El-ItsMe <elnazteymurova001@gmail.com>
2022-10-28 01:32:08 +0400

    added new folder and file "BiomVariants"

---
+++ ProjectClass1_Zoo/ProjectClass1_Zoo/Options/BiomVariants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClass1_Zoo.Options
+{
+    public enum BiomVariants
+    {
+        Tropics,
+        Tundra,
+        Taiga,
+        Steppes,
+        Savannahs,
+        Deserts, //пустыня
+        Tropical_rain_forests,
+        Winter_green_forests,
+        Jungle
+    }
+}
