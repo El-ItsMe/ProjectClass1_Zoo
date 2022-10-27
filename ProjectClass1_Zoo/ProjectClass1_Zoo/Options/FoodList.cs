5c3eb1bf8e98d5d9e3975dccfe92db4cabb578fd
El-ItsMe <elnazteymurova001@gmail.com>
2022-10-28 01:36:01 +0400

    Added new file "FoodList" in folder

---
+++ ProjectClass1_Zoo/ProjectClass1_Zoo/Options/FoodList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClass1_Zoo.Options
+{
+    public enum FoodList
+    {
+        Bamboo
+    }
+}
